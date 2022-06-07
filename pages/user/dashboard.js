7348509f21b86a09898199b62eb379a7b20fd825
William Dias <smdwilliam89@gmail.com>
2022-06-07 15:58:04 +0100

    Adicionando My Adds e Button

--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -1,9 +1,13 @@
-import { Container, Typography, styled } from '@mui/material';
+import { Container, Typography, Button, styled } from '@mui/material';
 import TemplateDefault from '../../src/templates/Default';
 
 const useStyles = styled((theme) => ({
   container: {
     padding: theme.spacing(8, 0, 6),
+  },
+  buttonAdd: {
+    margin: '30px auto',
+    display: 'block',
   },
 }));
 
@@ -19,9 +23,17 @@ export default function Home() {
         <Typography
           component='h1'
           variant='h2'
+          align='center'
         >
           My Adds
         </Typography>
+        <Button
+          variant='contained'
+          color='primary'
+          className={classes.buttonAdd}
+        >
+          Publish new ad
+        </Button>
       </Container>
     </TemplateDefault>
   );
