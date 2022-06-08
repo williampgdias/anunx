d21dc561ec711de91ea040693c5ca40091a49760
William Dias <smdwilliam89@gmail.com>
2022-06-08 18:02:32 +0100

    makeStyles resolvido

--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -1,9 +1,23 @@
-import { Container, Typography, Button, styled } from '@mui/material';
+import {
+  Typography,
+  Button,
+  Container,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+} from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
 import TemplateDefault from '../../src/templates/Default';
 
-const useStyles = styled((theme) => ({
+const useStyles = makeStyles((theme) => ({
   container: {
     padding: theme.spacing(8, 0, 6),
+  },
+  cardMedia: {
+    paddingTop: '56%',
   },
   buttonAdd: {
     margin: '30px auto',
@@ -34,6 +48,161 @@ export default function Home() {
         >
           Publish new ad
         </Button>
+      </Container>
+      <Container maxWidth='md'>
+        <Grid
+          container
+          spacing={4}
+        >
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={4}
+          >
+            <Card>
+              <CardMedia
+                className={classes.cardMedia}
+                image={'https://source.unsplash.com/random'}
+                title='Post title'
+              />
+              <CardContent>
+                <Typography
+                  variant='h5'
+                  component='h2'
+                >
+                  Product X
+                </Typography>
+                <Typography>€60</Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  size='small'
+                  color='primary'
+                >
+                  Edit
+                </Button>
+                <Button
+                  size='small'
+                  color='primary'
+                >
+                  Remove
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={4}
+          >
+            <Card>
+              <CardMedia
+                className={classes.cardMedia}
+                image={'https://source.unsplash.com/random'}
+                title='Post title'
+              />
+              <CardContent>
+                <Typography
+                  variant='h5'
+                  component='h2'
+                >
+                  Product X
+                </Typography>
+                <Typography>€60</Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  size='small'
+                  color='primary'
+                >
+                  Edit
+                </Button>
+                <Button
+                  size='small'
+                  color='primary'
+                >
+                  Remove
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={4}
+          >
+            <Card>
+              <CardMedia
+                className={classes.cardMedia}
+                image={'https://source.unsplash.com/random'}
+                title='Post title'
+              />
+              <CardContent>
+                <Typography
+                  variant='h5'
+                  component='h2'
+                >
+                  Product X
+                </Typography>
+                <Typography>€60</Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  size='small'
+                  color='primary'
+                >
+                  Edit
+                </Button>
+                <Button
+                  size='small'
+                  color='primary'
+                >
+                  Remove
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={4}
+          >
+            <Card>
+              <CardMedia
+                className={classes.cardMedia}
+                image={'https://source.unsplash.com/random'}
+                title='Post title'
+              />
+              <CardContent>
+                <Typography
+                  variant='h5'
+                  component='h2'
+                >
+                  Product X
+                </Typography>
+                <Typography>€60</Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  size='small'
+                  color='primary'
+                >
+                  Edit
+                </Button>
+                <Button
+                  size='small'
+                  color='primary'
+                >
+                  Remove
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
       </Container>
     </TemplateDefault>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
 export default function ButtonAppBar() {
